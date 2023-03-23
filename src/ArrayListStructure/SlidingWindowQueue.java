5b127ca92820653ef20eb6a8c974d5b2bad382c0
안상형 <shahn0718@gmail.com>
2023-03-23 17:01:07 +0900

    20230323 슬라이딩윈도우

--- src/ArrayListStructure/SlidingWindowQueue.java
+++ src/ArrayListStructure/SlidingWindowQueue.java
@@ -1,18 +1,41 @@
 package ArrayListStructure;
 
-public class SlidingWindowQueue {
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class SlidingWindowQueue {
     /**
-     *
      * QUEUE 성질
      */
 
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
 
-
-
-
-    public static void main(String[] args) {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     }
 }
+
+//        Queue<String> que = new LinkedList<>();
+//
+//        que.add("1st");
+//        que.add("2th");
+//        que.add("3th");
+//        que.add("4th");
+//        que.add("5th");
+//
+//        System.out.println(que);
+//
+//        que.poll();
+//        System.out.println(que);
+//
+//        //que.element는 que의 머리
+//        //que.element(); , que.peek(); - 삭제하지않지만, 자료는 남는다.
+
