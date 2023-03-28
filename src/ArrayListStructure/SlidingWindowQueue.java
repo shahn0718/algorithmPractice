e681fe8eb90162479b8c8f2323450b606073e4ec
안상형 <shahn0718@gmail.com>
2023-03-28 13:56:35 +0900

    20230328 슬라이딩윈도우&큐

--- src/ArrayListStructure/SlidingWindowQueue.java
+++ src/ArrayListStructure/SlidingWindowQueue.java
@@ -1,12 +1,21 @@
 package ArrayListStructure;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class SlidingWindowQueue {
+
+    static class Node{
+        private int value;
+        private int index;
+
+        Node(int value, int index){
+            this.value = value;
+            this.index = index;
+        }
+    }
+
+
     /**
      * QUEUE 성질
      */
@@ -16,9 +25,28 @@ public class SlidingWindowQueue {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        Deque<Node> deque = new LinkedList<>();
+        for(int i=0; i<N; i++){
+            int tmp = Integer.parseInt(st.nextToken());
+
+            while(!deque.isEmpty() && deque.getLast().value > tmp){
+                deque.removeLast();
+            }
+            deque.addLast(new Node(tmp,i));
+            if(deque.getFirst().index <= i-L){
+                deque.removeFirst();
+            }
+            bw.write(deque.getFirst().value+ " ");
+        }
+        bw.flush();
+        bw.close();
 
     }
 }
